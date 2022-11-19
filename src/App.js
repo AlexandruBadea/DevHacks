55e46cb93175deb095a0936ae0ec67bfd36d0eb6
AlexandruBadea <badeaalexsandu@gmail.com>
2022-11-19 19:20:33 +0200

    moving div

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import * as React from "react";
+import { motion } from "framer-motion";
+
 import './App.css';
+
+// move div to the right
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    // div moves to the right
+    <motion.div
+      initial={{ x: 0 }}
+      animate={{ x: 100 }}
+      transition={{ duration: 2 }}
+    >
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </motion.div>
+
   );
 }
 
 export default App;
+
